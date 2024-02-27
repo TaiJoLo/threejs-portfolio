c3e05471101e5f9fdb3ced9c9e1e615811c6484d
TaiJoLo <lotaijo@gmail.com>
2024-02-27 22:23:22 +1300

    Project Page and adding sound

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,72 @@
 import React from "react";
+import { projects } from "../constants";
+import { Link } from "react-router-dom";
+import { arrow } from "../assets/icons";
+import CTA from "../components/CTA";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        My{" "}
+        <span className="blue-gradient_text  font-semibold drop-shadow">
+          Projects
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Welcome to the heart of my digital journey! Here, lines of code
+          transcend into dynamic projects, each a testament to my passion for
+          innovation and precision in web development. Explore a curated
+          collection where creativity meets functionality, and concepts
+          transform into tangible digital experiences. From responsive web
+          designs to intuitive applications, join me on a visual odyssey through
+          the diverse landscapes of my digital portfolio. Each project
+          encapsulates not just code but a narrative of problem-solving,
+          creativity, and the relentless pursuit of excellence. Dive in and
+          discover the artistry of technology in every pixel and line of code.
+        </p>
+      </div>
+      <div className="flex flex-wrap my-20 gap-16">
+        {projects.map((project) => (
+          <div className="lg:w-[400px] w-full" key={project.name}>
+            <div className="block-container w-12 h-12">
+              <div className={`btn-back rounded-xl ${project.theme}`} />
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <img
+                  src={project.iconUrl}
+                  alt="Project Icon"
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+            <div className="mt-5 flex flex-col">
+              <h4 className="text-2xl font-poppins font-semibold">
+                {project.name}
+              </h4>
+              <p className="mt-2 text-slate-500">{project.description}</p>
+              <div className="mt-5 flex tiems-center gap-2 font-poppins">
+                <Link
+                  to={project.link}
+                  target="noopener noreferrer"
+                  className="font-semibold text-blue-600"
+                >
+                  Live Link
+                </Link>
+                <img
+                  src={arrow}
+                  alt="arrow"
+                  className="w-4 h-4 object-contain"
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <hr className="border-slate-200" />
+      <CTA />
+    </section>
+  );
 };
 
 export default Projects;
